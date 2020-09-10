583eaed822288b2836dc8aa0e471c882a9ec0bee
MattGogan <55172084+MattGogan@users.noreply.github.com>
2020-09-10 06:07:35 -0400

    refactoring

--- js/quiz.js
+++ js/quiz.js
@@ -13,7 +13,16 @@ function winloaded(){
     document.body.appendChild( renderer.domElement );
 
 
-    var geometry = new THREE.BoxGeometry(); var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } ); var cube = new THREE.Mesh( geometry, material ); scene.add( cube ); camera.position.z = 5;
+    var geometry = new THREE.BoxGeometry(); 
+    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } ); 
+    var cube = new THREE.Mesh( geometry, material ); 
+    scene.add( cube ); camera.position.z = 5;
+
+
+    var welcomeboxGeom = new THREE.geometry();
+    var welcomeboxMaterial = new THREE.MeshBasicMaterial(); //add a picture of the text here.
+    
+    var welcomeboxMesh = new THREE.Mesh();
 
     var welcomeMessage = "Welcome to Lauren's Tree Quiz";
 
