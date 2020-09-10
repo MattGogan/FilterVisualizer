8ed07040064ce3343f395f6093f2aee8120f445b
MattGogan <55172084+MattGogan@users.noreply.github.com>
2020-09-10 07:27:02 -0400

    textsprites done

--- js/quiz.js
+++ js/quiz.js
@@ -1,3 +1,5 @@
+//const { Loader } = require("./three");
+
 window.onload = function(){
     console.log("Bazooper");
     winloaded();
@@ -5,9 +7,17 @@ window.onload = function(){
 
 
 function winloaded(){
+    executethree();
+}
+
+
+function executethree(){
+
+
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
     var renderer = new THREE.WebGLRenderer();
+    camera.position.z = 20;
 
     renderer.setSize( window.innerWidth, window.innerHeight ); 
     document.body.appendChild( renderer.domElement );
@@ -16,25 +26,54 @@ function winloaded(){
     var geometry = new THREE.BoxGeometry(); 
     var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } ); 
     var cube = new THREE.Mesh( geometry, material ); 
-    scene.add( cube ); camera.position.z = 5;
+    cube.position.x +=3;
+    cube.position.y -=3;
+    scene.add( cube ); 
+
+    let sprite = new THREE.TextSprite({
+        fillStyle: '#24ff00',
+        fontFamily: '"Times New Roman", Times, serif',
+        fontSize: 10,
+        fontStyle: 'italic',
+        text: [
+          'Some Test Font'
+        ].join('\n'),
+      });
+      scene.add(sprite);
+      sprite.fontSize = 5;
 
 
-    var welcomeboxGeom = new THREE.geometry();
-    var welcomeboxMaterial = new THREE.MeshBasicMaterial(); //add a picture of the text here.
+      let messages = [
+          "Carbon Footprint Questionnaire",
+          "More Trees?",
+          "Even more trees?"   
+      ]
     
-    var welcomeboxMesh = new THREE.Mesh();
+      let textobjects = [];
 
-    var welcomeMessage = "Welcome to Lauren's Tree Quiz";
+      for(i = 0; i<messages.length; i++){
+        
+        textobjects[i] = new THREE.TextSprite({
+        fillStyle: '#24ff00',
+        fontFamily: '"Times New Roman", Times, serif',
+        fontSize: 10,
+        fontStyle: 'italic',
+        text: [
+            messages[i]
+            ].join('\n'),
+        });
 
-    var loader = new THREE.FontLoader();
+      }
 
-    loader.load()
-    
+
+    scene.add(textobjects[1]);
+
     function animate() { 
         requestAnimationFrame(animate); 
         renderer.render( scene, camera );
         
-        
+        sprite.position.x +=.1;
+
         cube.rotation.x += 0.01;
         cube.rotation.y += 0.01;
 
@@ -42,6 +81,5 @@ function winloaded(){
     }
 
     animate();
+
 }
-
-
