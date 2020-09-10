070e3de0041279435dfc26190d815133d981ffb9
MattGogan <55172084+MattGogan@users.noreply.github.com>
2020-09-10 09:22:35 -0400

    progress

--- js/quiz.js
+++ js/quiz.js
@@ -1,5 +1,9 @@
 //const { Loader } = require("./three");
 
+let treeMeshes = [];
+let treecount = 0;
+var scene = new THREE.Scene();
+
 window.onload = function(){
     console.log("Bazooper");
     winloaded();
@@ -7,40 +11,21 @@ window.onload = function(){
 
 
 function winloaded(){
-    executethree();
+    treeMeshes = [];
+    executethree(); 
 }
+
 
 
 function executethree(){
 
-
-    var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
     var renderer = new THREE.WebGLRenderer();
     camera.position.z = 20;
 
-    renderer.setSize( window.innerWidth, window.innerHeight ); 
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setClearColor(0xffffff, 1); 
     document.body.appendChild( renderer.domElement );
-
-
-    var geometry = new THREE.BoxGeometry(); 
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } ); 
-    var cube = new THREE.Mesh( geometry, material ); 
-    cube.position.x +=3;
-    cube.position.y -=3;
-    scene.add( cube ); 
-
-    let sprite = new THREE.TextSprite({
-        fillStyle: '#24ff00',
-        fontFamily: '"Times New Roman", Times, serif',
-        fontSize: 10,
-        fontStyle: 'italic',
-        text: [
-          'Some Test Font'
-        ].join('\n'),
-      });
-      scene.add(sprite);
-      sprite.fontSize = 5;
 
 
       let messages = [
@@ -50,13 +35,15 @@ function executethree(){
       ]
     
       let textobjects = [];
+      
+      
 
       for(i = 0; i<messages.length; i++){
         
         textobjects[i] = new THREE.TextSprite({
-        fillStyle: '#24ff00',
+        fillStyle: '#00000',
         fontFamily: '"Times New Roman", Times, serif',
-        fontSize: 10,
+        fontSize: 5,
         fontStyle: 'italic',
         text: [
             messages[i]
@@ -65,21 +52,41 @@ function executethree(){
 
       }
 
-
+    textobjects[0].position.y+=10;
+    textobjects[0].position.x-=20;
+    scene.add(textobjects[0]);
     scene.add(textobjects[1]);
 
     function animate() { 
         requestAnimationFrame(animate); 
         renderer.render( scene, camera );
         
-        sprite.position.x +=.1;
+        textobjects[0].position.x +=.1;
+        if(textobjects[0].position.x > 25 && textobjects[1].position.y<10){
+            textobjects[1].position.y+=.05;
+        }
 
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
-
+        for(i = 0; i<treeMeshes.length; i++){
+            treeMeshes[i].rotation.y+=.01;
+        }
 
     }
 
     animate();
 
+}
+
+
+function addTree(){
+    var geometry = new THREE.BoxGeometry(); 
+
+    //var loader = new OBJLoader();
+
+    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } ); 
+    var cube = new THREE.Mesh( geometry, material ); 
+    cube.position.z = -5;
+    cube.position.x += Math.random()*40-20;
+    cube.position.y += Math.random()*30-15;
+    treeMeshes.push(cube);
+    scene.add(cube);
 }
